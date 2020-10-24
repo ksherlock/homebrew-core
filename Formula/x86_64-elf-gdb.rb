843ca294aac00dd27e984238d7c1bc8915853772
Kelvin Sherlock <ksherlock@gmail.com>
2020-10-24 17:03:31 -0400

    x86_64-elf-gdb 10.1

--- Formula/x86_64-elf-gdb.rb
+++ Formula/x86_64-elf-gdb.rb
@@ -1,8 +1,8 @@
 class X8664ElfGdb < Formula
   desc "GNU debugger for i386-elf cross development"
   homepage "https://www.gnu.org/software/gdb/"
-  url "https://ftp.gnu.org/gnu/gdb/gdb-9.2.tar.xz"
-  sha256 "360cd7ae79b776988e89d8f9a01c985d0b1fa21c767a4295e5f88cb49175c555"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-10.1.tar.xz"
+  sha256 "f82f1eceeec14a3afa2de8d9b0d3c91d5a3820e23e0a01bbb70ef9f0276b62c0"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -11,7 +11,7 @@ class X8664ElfGdb < Formula
     sha256 "08a7d238e78d7cda369afbf7e00e5e778a6d089c7c28713da8eb81692525c849" => :high_sierra
   end
 
-  depends_on "python@3.8"
+  depends_on "python@3.9"
   depends_on "xz"
 
   conflicts_with "gdb", because: "both install include/gdb, share/gdb and share/info"
